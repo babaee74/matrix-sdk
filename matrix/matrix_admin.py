3ffbdbef70fd55896e53172b4c0a5bc671e7b5e0
babaee74 <60437337+babaee74@users.noreply.github.com>
2024-03-11 03:17:44 +0330

    simplifying command

--- matrix/matrix_admin.py
+++ matrix/matrix_admin.py
@@ -8,7 +8,7 @@ def parse_cmd() -> dict:
     parser = argparse.ArgumentParser(
                         prog='Matrix Project Admin',
                         description='This script is Matrix Admin and it is used to do different task related for matrix including creating a new project')
-    parser.add_argument('--start_project', action='store_true', help="If given, it will setup a project")
+    parser.add_argument('--startproject', action='store_true', help="If given, it will setup a project")
     args = parser.parse_args()
     
     return vars(args)
