2ca8ab1d2a06edead49c28b3371901a58506253f
babaee74 <60437337+babaee74@users.noreply.github.com>
2024-06-30 03:58:05 +0330

    updated server uri for client

--- matrix/client/server.py
+++ matrix/client/server.py
@@ -1,4 +1,4 @@
-SERVER_URI = "https://api.icanapp.name"
+SERVER_URI = "https://api.matrix-ai.app"
 UPLOAD_FILES_URI = "/upload/files/"
 MODEL_REQUEST_URI = "/repo/model/"
 REQUEST_RESULT_URI = "/repo/results/"
